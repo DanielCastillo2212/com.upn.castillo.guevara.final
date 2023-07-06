3bb3adf7bbfc0e1bcbb3c84daab04fec8519b71c
DanielCastillo2212 <danielandre.castillo@gmail.com>
2023-07-06 12:38:08 -0500

    Ultimo push

--- app/src/main/java/com/example/comupncastilloguevarafinal/RegistrarDuelistaActivity.java
+++ app/src/main/java/com/example/comupncastilloguevarafinal/RegistrarDuelistaActivity.java
@@ -11,7 +11,14 @@ import android.widget.Toast;
 
 import com.example.comupncastilloguevarafinal.DB.AppDatabase;
 import com.example.comupncastilloguevarafinal.Entities.Duelista;
+import com.example.comupncastilloguevarafinal.Services.DuelistaApiService;
 import com.example.comupncastilloguevarafinal.Services.DuelistaDao;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegistrarDuelistaActivity extends AppCompatActivity {
     private EditText etNombre;
@@ -48,14 +55,19 @@ public class RegistrarDuelistaActivity extends AppCompatActivity {
 
         Duelista duelista = new Duelista(nombre);
 
-        // Ejecutar la operación en un hilo separado
+        // Ejecutar la operación en un hilo separado utilizando AsyncTask
         new InsertDuelistaTask().execute(duelista);
     }
 
     private class InsertDuelistaTask extends AsyncTask<Duelista, Void, Long> {
+        private Duelista duelista;
+
         @Override
         protected Long doInBackground(Duelista... duelistas) {
-            // Insertar el duelistas en la base de datos y devolver el ID generado
+            // Guardar el Duelista recibido en la variable de instancia
+            duelista = duelistas[0];
+
+            // Insertar el Duelista en la base de datos y devolver el ID generado
             return duelistaDao.insertDuelista(duelistas[0]);
         }
 
@@ -63,12 +75,50 @@ public class RegistrarDuelistaActivity extends AppCompatActivity {
         protected void onPostExecute(Long duelistaId) {
             super.onPostExecute(duelistaId);
 
-            if (duelistaId != null) {
-                Toast.makeText(RegistrarDuelistaActivity.this, "Duelista registrado con ID: " + duelistaId, Toast.LENGTH_SHORT).show();
-                etNombre.setText("");
+            if (duelistaId != -1) {
+                // Duelista insertado correctamente en la base de datos local
+
+                // Crear una instancia de Retrofit con la configuración adecuada
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://64a5ba5100c3559aa9c01d7a.mockapi.io/") // Reemplazar con la URL base de tu API MockAPI
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                // Crear el servicio de la API
+                DuelistaApiService apiService = retrofit.create(DuelistaApiService.class);
+
+                // Realizar la llamada para crear el Duelista en la API
+                Call<Duelista> call = apiService.createDuelista(duelista);
+                call.enqueue(new Callback<Duelista>() {
+                    @Override
+                    public void onResponse(Call<Duelista> call, Response<Duelista> response) {
+                        if (response.isSuccessful()) {
+                            Duelista createdDuelista = response.body();
+                            if (createdDuelista != null) {
+                                // Mostrar el mensaje de éxito
+                                Toast.makeText(RegistrarDuelistaActivity.this, "Duelista registrado con ID: " + createdDuelista.getId(), Toast.LENGTH_SHORT).show();
+                                etNombre.setText("");
+                            } else {
+                                // Mostrar un mensaje de error
+                                Toast.makeText(RegistrarDuelistaActivity.this, "Error al registrar el Duelista en MockAPI", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            // Mostrar un mensaje de error
+                            Toast.makeText(RegistrarDuelistaActivity.this, "Error al registrar el Duelista en MockAPI", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Duelista> call, Throwable t) {
+                        // Mostrar un mensaje de error
+                        Toast.makeText(RegistrarDuelistaActivity.this, "Error al registrar el Duelista en MockAPI", Toast.LENGTH_SHORT).show();
+                    }
+                });
             } else {
-                Toast.makeText(RegistrarDuelistaActivity.this, "Error al registrar el Duelista", Toast.LENGTH_SHORT).show();
+                // Error al insertar el Duelista en la base de datos local
+                Toast.makeText(RegistrarDuelistaActivity.this, "Error al registrar el Duelista en la base de datos local", Toast.LENGTH_SHORT).show();
             }
         }
     }
-}
+    }
+
